dc970d1c40f2cca658989a354f4fb295d020bc34
binfoo <709875055@qq.com>
2023-11-05 21:33:13 +0800

    Update diy.sh
    新增主题edge和design

--- diy.sh
+++ diy.sh
@@ -3,3 +3,7 @@
 # git clone https://github.com/EOYOHOO/UA2F.git package/UA2F
 # git clone https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
 # git clone https://github.com/kongfl888/luci-app-timedreboot package/timedreboot
+git clone https://github.com/kiddin9/luci-app-wizard.git package/luci-app-wizard
+git clone -b 18.06 https://github.com/kiddin9/luci-theme-edge.git package/luci-theme-edge
+git clone https://github.com/gngpp/luci-app-design-config.git package/luci-app-design-config
+git clone https://github.com/gngpp/luci-theme-design.git  package/luci-theme-design
